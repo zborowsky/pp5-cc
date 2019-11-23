a8ffe2fd4acf558d0f7420b2e51482fcec453960
s-115-33 <s-115-33@gumiforest.ae.krakow.pl>
2019-11-23 14:21:26 +0100

    more fancy hello world stmt

--- src/main/java/pl/jkon/pp5/creditcard/Application.java
+++ src/main/java/pl/jkon/pp5/creditcard/Application.java
@@ -2,6 +2,6 @@ package pl.jkon.pp5.creditcard;
 
 public class Application {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World :)");
     }
 }
